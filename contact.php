f36f71d11a8c0176f9e3a1ce0b1fae676e6c9875
novatechsolution <novatechno.id@gmail.com>
2025-05-20 00:41:57 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,68 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+// var_dump($message);
+// die;
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) && isset($_POST['subject'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $subject = $_POST['subject'];
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer;
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    //Ask for HTML-friendly debug output
+    $mail->Debugoutput = 'html';
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.gmail.com';
+    // use
+    // $mail->Host = gethostbyname('smtp.gmail.com');
+    // if your network does not support SMTP over IPv6
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "mail@gmail.com";
+    //Password to use for SMTP authentication
+    $mail->Password = "password";
+    //Set who the message is to be sent from
+    $mail->setFrom('bittekno.id@gmail.com', 'Bitekno');
+    //Set an alternative reply-to address
+    $mail->addReplyTo('bittekno.id@gmail.com', 'Bitekno');
+    //Set who the message is to be sent to
+    $mail->addAddress('juangsabit7@gmail.com', 'Juang');
+    //Set the subject line
+    $mail->Subject = $subject;
+    //Replace the plain text body with one created manually
+    $body = "<html>"; 
+    $body .= "<body style=\"font-family:Verdana, Verdana, Geneva, sans-serif; font-size:12px; color:#666666;\">"; 
+    $body .= 'Nama = '.$name.'<br>';
+    $body .= 'Email = '.$email.'<br>';
+    $body .= 'Pesan = '.$message.'<br>';
+    $body .= "</body><br>"; 
+    $body .= "</html><br>";
+    $mail->IsHTML(true);
+    $mail->Body = $body;
+    //send the message, check for errors
+    if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+    echo "Message sent!";
+    }
+} else {
+    header('location:index.html');
+}
---
+++ test.php
@@ -0,0 +1,50 @@
+<?php
+date_default_timezone_set('Etc/UTC');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "cryptexyz@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "Pengertian!((*";
+//Set who the message is to be sent from
+$mail->setFrom('from@example.com', 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('juangsabit7@gmail.com', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+//Replace the plain text body with one created manually
+$mail->Body = 'This is a plain-text message body';
+//send the message, check for errors
+if (!$mail->send()) {
+ echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+ echo "Message sent!";
+}
